b8612e457b1603cf33d666b5002c6e6e5f9b4860
Matthew <mattsjsu14@gmail.com>
2018-07-06 20:14:45 -0700

    worked on note modal

--- public/assets/js/articles.js
+++ public/assets/js/articles.js
@@ -1,40 +1,99 @@
 $(function(){
 
 
-  // this function will handle to following:
-  // if there are no saved articles, display default div containing message "You haven't saved any articles!" 
-  // if there are articles remove default div and show article card(s)
+  
 
   $('.viewNotes').on("click", function(e) {
     e.preventDefault();
     const $id = $(this).data("id"); // `this` refers to the element that triggered the event because we use a normal function expression and not () => {}
-    const modal = [`
-        <div class="modal" tabindex="-1" role="dialog" style="display: block;">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title">Notes for: ${$id}</h5>           
+    console.log($id);
+
+    
+    
+    $.ajax({
+      method:'GET',
+      url: "/articles/" + $id,       
+    }).then(function(data){
+
+      console.log(data);
+
+      const notes = data.note;
+
+      const showNotes = function() {
+        let allNotes = [];
+
+        if (notes.length !== 0) {
+          allNotes = notes.map(function(note) {
+            return `<li class="list-group-item">${note.body}</li>`;
+          });
+        } else {
+          return `<li class="list-group-item">No notes yet.</li>`;
+        }
+
+        return allNotes.join("");
+      }
+
+
+        //default modal if there are no notes
+        const modal = [`
+            <div class="modal" tabindex="-1" role="dialog" style="display: block;">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title">Notes for: ${$id}</h5>           
+                  </div>
+                  <div class="modal-body">
+                    <ul class="list-group note-container">
+                      ${showNotes()}
+                    </ul>
+                    <textarea class="userNote" placeholder="New note goes here" style="padding: 10px; width: 100%; resize:none; margin-top:10px;"></textarea>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-danger saveNote" data-id="${$id}">Save</button>
+                    <button type="button" class="btn btn-secondary closeNote" data-dismiss="modal">Close</button>
+                  </div>
+                </div>
               </div>
-              <div class="modal-body">
-                <ul class="list-group note-container">
-                  <li class="list-group-item">No notes yet.</li>
-                </ul>
-                <textarea placeholder="New note goes here" style="padding: 10px; width: 100%; resize:none; margin-top:10px;"></textarea>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-danger saveNote">Save</button>
-                <button type="button" class="btn btn-secondary closeNote" data-dismiss="modal">Close</button>
-              </div>
-            </div>
           </div>
-      </div>
-      `];
-      // make modal pop up on page 
-      $("#container").append(modal);
+          `];
+
+
+        $("#container").append(modal);
+      });
+
+
   })
 
-  // $(document).on('click', '.saveNote', function(){
 
-  // })
+  // functionality for save button on note modal
+  $(document).on('click', '.saveNote', function(e){
+    e.preventDefault();
+
+    const userNote = $('.userNote').val()
+    const articleId = $(this).data("id")
+
+    $.ajax({
+      method: 'POST',
+      url: "/articles/" + articleId,
+      data: {
+        body: userNote
+      }
+    }).then(function(){
+      //clear note from textarea
+      $('.userNote').val("")
+
+      //remove default note. (In progress)
+      // $('.list-group-item').remove()
+
+      //store <li> so we can append it to .note-container section of the modal
+      const noteListItem = [`
+      <li class="list-group-item">${userNote}</li>
+      `]
+      //append new note
+      $('.note-container').append(noteListItem)
+
+
+    });
+  });
 
 })
--- server.js
+++ server.js
@@ -35,10 +35,10 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost:27017/scraperDB");
+// mongoose.connect("mongodb://localhost:27017/scraperDB");
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/scraperDB";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
@@ -92,6 +92,22 @@ app.get("/api/scrape", function(req, res){
   })
 })
 
+
+
+// Route for clearing articles in db
+app.delete('/api/deletearticle:id', function(req, res){
+  db.Note.findByIdAndRemove(req.params.id, (err, todo) => {
+    if (err) return res.status(500).send(err)
+    // send back with a message and the id of the document that was removed
+    const response = { 
+      message: "Article successfully deleted", 
+      id: article._id
+    }
+    return res.status(200).send(response)
+  })
+})
+
+
 // Route for saving an article
 app.post("/api/save", function(req, res){
   db.Article.create(req.body)
@@ -109,8 +125,7 @@ app.post("/api/save", function(req, res){
 app.get("/articles", function(req, res){
   db.Article.find({})
     .then(function(dbArticles){
-      // If we were able to successfully find Articles, send them back to the client
-      // res.json(dbArticle)
+      //store data in handlebars object
       const handlebarObj = {articles: dbArticles}
       console.log(dbArticles)
       res.render('articles', handlebarObj)
@@ -118,7 +133,97 @@ app.get("/articles", function(req, res){
     
 })
 
+app.post("/note", (req, res) => {
+  db.Note.create({ title: "test", body: "test body" }).then(note => console.log(note))
+})
 
+
+//Route for getting saved notes
+// app.get("/notes", function(req, res){
+//   db.Note.find()
+//     .then(function(dbNotes){
+//       //store data in handlebars object
+//       const handlebarObj = {notes: dbNotes}
+//       console.log(dbNotes)
+//       res.render('articles', handlebarObj )
+//     })
+// })
+
+//Route for creating a new note
+// app.post('/api/newnote', function(req, res){
+//   db.Note.create(req.body)
+//     .then(function(dbNotes){
+//       console.log(dbNotes)
+//       res.send(dbNotes)
+//     })
+//     .catch(function(err){
+//       return res.json(err)
+//     })
+// })
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, store data in handlebars object
+      // const handlebarObj = {notes: dbNotes}
+      // console.log(dbNotes)
+      res.send(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for creating/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+//Route for deleting a note
+app.delete('/api/deletenote:id', function(req, res){
+  db.Note.findByIdAndRemove(req.params.id, (err, todo) => {
+    if (err) return res.status(500).send(err)
+    // send back with a message and the id of the document that was removed
+    const response = { 
+      message: "Note successfully deleted", 
+      id: note._id
+    }
+    return res.status(200).send(response)
+  })
+})
+
+//Route for deleting a saved article
+app.delete('/api/deletearticle:id', function(req, res){
+  db.Note.findByIdAndRemove(req.params.id, (err, todo) => {
+    if (err) return res.status(500).send(err)
+    // send back with a message and the id of the document that was removed
+    const response = { 
+      message: "Article successfully deleted", 
+      id: article._id
+    }
+    return res.status(200).send(response)
+  })
+})
 
 
 // Start the server
